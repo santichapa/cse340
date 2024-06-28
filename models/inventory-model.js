216524748c9c166b41c17f903047559104b88909
santichapa <santi.chaparro280@gmail.com>
2024-06-28 11:41:49 -0300

    fixed error in query

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -14,7 +14,7 @@ async function getInventoryByClassificationId(classification_id) {
   try {
     const data = await pool.query(
       `SELECT * FROM public.inventory AS i
-      JOIN public.classification_id AS c
+      JOIN public.classification AS c
       ON i.classification_id = c.classification_id
       WHERE i.classification_id = $1`,
     [classification_id]
