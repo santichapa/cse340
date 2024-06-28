e8da99e7183633753dbc8f19192e87be4aefd92a
santichapa <santi.chaparro280@gmail.com>
2024-06-28 00:49:23 -0300

    fixed learning activity 2

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ****************************
  * View Engine and Templates
@@ -24,8 +25,11 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 
-//Index route
+// Index route
 app.get("/", baseController.buildHome)
+
+// Inventory Routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
