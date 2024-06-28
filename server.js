ba0db7dae65b2ea96d3b79e30308dd83d9151297
santichapa <santi.chaparro280@gmail.com>
2024-06-28 19:31:38 -0300

    robust error handler implemented

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 // Index route
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // Inventory Routes
 app.use("/inv", inventoryRoute)
@@ -43,9 +43,14 @@ app.use(async (req, res, next) => {
 app.use(async (err, req, res, next) => {
   let nav = await utilities.getNav()
   console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if (err.status == 404) {
+    message = err.message
+  } else {
+    message = 'Oh no! There was a crash. Maybe try a different route?'
+  }
   res.render("errors/error", {
     title: err.status || 'Server Error', 
-    message: err.message,
+    message,
     nav
   })
 })
